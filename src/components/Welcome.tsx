9b44c7c451d80dda7e6d3373feec8d7600842ffa
Alexis HELM <128365251+Lydovw@users.noreply.github.com>
2025-03-13 08:48:37 +0100

    delete unuse files

--- src/components/Welcome.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Welcome = () => {
-  return (
-    <h1 className='text-2xl font-semibold'>Welcome on Blogito, please sign in</h1>
-  )
-}
-
-export default Welcome
